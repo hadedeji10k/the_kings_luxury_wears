830edb9535e882f3119db2ee98afb87283c94985
Adedeji Yusuf <adedejiyusuf26@gmail.com>
2021-02-13 13:39:48 +0100

    After deployment

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: "mongodb://localhost:27017/store"
-    // database: "mongodb+srv://root:root1@cluster0.6yrwn.mongodb.net/storecart?retryWrites=true&w=majority"
-}
+  // database: "mongodb://localhost:27017/store"
+  database:
+    "mongodb+srv://root:root1@cluster0.6yrwn.mongodb.net/storecart?retryWrites=true&w=majority",
+};
