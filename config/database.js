43f6d8e12ee3699e4342a7ef48743cdf348163eb
Adedeji Yusuf Yinka <59361465+hadedeji10k@users.noreply.github.com>
2021-02-14 06:02:34 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 module.exports = {
   // database: "mongodb://localhost:27017/store"
-  database:
-    "mongodb+srv://root:root1@cluster0.6yrwn.mongodb.net/storecart?retryWrites=true&w=majority",
+  //database:
+//"mongodb+srv://root:root1@cluster0.6yrwn.mongodb.net/storecart?retryWrites=true&w=majority",
+database: "mongodb+srv://root:morenikeji@26@the-kings.6yrwn.mongodb.net/the-kings?retryWrites=true&w=majority"
+
 };
