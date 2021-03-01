3bc02f7e8c1d3320518193820efb16d42c21f269
Adedeji Yusuf <adedejiyusuf26@gmail.com>
2021-03-01 12:54:15 +0100

    After Admin Pagination

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-  database: "mongodb://localhost:27017/store",
+  // database: "mongodb://localhost:27017/store",
   // database:    "mongodb+srv://root:root1@cluster0.6yrwn.mongodb.net/Store?retryWrites=true&w=majority",
-  // database:
-  //   "mongodb+srv://root:morenikeji@26@the-kings.6yrwn.mongodb.net/the-kings?retryWrites=true&w=majority",
+  database:
+    "mongodb+srv://root:morenikeji@26@the-kings.6yrwn.mongodb.net/the-kings?retryWrites=true&w=majority",
 };
